81eb8864fbd5fdd8039e9ea8331ffb12f7ce1649
Willa Walthall <ghthor@gmail.com>
2015-02-06 14:11:09 -0500

    Some notes about where this is heading

--- www/main_test.go
+++ www/main_test.go
@@ -13,8 +13,13 @@ import (
 func DescribeClient(c gospec.Context) {
 	indexTmpl := template.Must(template.New("index.tmpl").ParseFiles("index.tmpl"))
 
+	// TODO add a route to the http server that phantom can trigger
+	// to signify that the tests are completed running
 	_, err := game.NewSimShard("localhost:45001", indexTmpl, "js/main_test")
 	c.Assume(err, IsNil)
+
+	// TODO verify that phantomjs is installed.
+	// It is the only external dependancy to run the client tests.
 }
 
 func TestClient(t *testing.T) {
