e904513453d27fd90d81a34d032fce64832d2e76
Willa Walthall <ghthor@gmail.com>
2015-02-11 15:44:41 -0500

    Document why this event is being listened too

--- www/js/loginUI.js
+++ www/js/loginUI.js
@@ -208,6 +208,7 @@ require([
     var loginSuccess = function(actor, socket) {
         var client = new Client(socket, actor);
 
+        // Wait for the CAAT director to prepare the canvas
         client.on("canvasReady", function(canvas) {
             react.render(react.DOM.div({id: "clientCanvas"}), document.body);
             $("#clientCanvas").append(canvas);
