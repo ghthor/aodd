b95090650a6465d6a6fe5d98f151d89693512a94
Willa Walthall <ghthor@gmail.com>
2015-02-12 18:45:01 -0500

    Cull and Diff the world state then ship is down the tubes

--- game/actor.go
+++ game/actor.go
@@ -35,13 +35,15 @@ type actorConn struct {
 	readCmdReq <-chan actorCmdRequest
 
 	// Comm interface to muxer used by SendState() method
-	sendState chan<- interface{}
+	sendState chan<- *rpg2d.WorldState
 
 	// Comm interface to muxer used by stopIO() method
 	stop chan<- chan<- struct{}
 
 	// External connection used to publish the world state
 	protocol.Conn
+
+	lastState rpg2d.WorldState
 }
 
 func newActorConn(conn protocol.Conn) actorConn {
@@ -57,7 +59,19 @@ type actorEntity struct {
 	cell   coord.Cell
 	facing coord.Direction
 
-	pathAction coord.PathAction
+	pathAction *coord.PathAction
+}
+
+type actorEntityState struct {
+	EntityId int64 `json:"id"`
+
+	Name string `json:"name"`
+
+	Facing string     `json:"facing"`
+	Cell   coord.Cell `json:"cell"`
+	bounds coord.Bounds
+
+	PathAction *coord.PathAction `json:"pathAction"`
 }
 
 type actor struct {
@@ -65,28 +79,63 @@ type actor struct {
 	actorConn
 }
 
-func (a actor) Entity() entity.Entity       { return a.actorEntity }
-func (a actor) WriteState(rpg2d.WorldState) {}
-
+func (a actor) Entity() entity.Entity  { return a.actorEntity }
 func (e actorEntity) Id() int64        { return e.id }
 func (e actorEntity) Cell() coord.Cell { return e.cell }
 func (e actorEntity) Bounds() coord.Bounds {
-	return coord.JoinBounds(coord.Bounds{
+	bounds := coord.Bounds{
 		e.cell,
 		e.cell,
-	}, e.pathAction.Bounds())
+	}
+
+	if e.pathAction != nil {
+		bounds = coord.JoinBounds(bounds, e.pathAction.Bounds())
+	}
+
+	return bounds
 }
 
-func (e actorEntity) ToState() entity.State { return e }
-func (e actorEntity) IsDifferentFrom(entity.State) bool {
-	return true
+func (e actorEntity) ToState() entity.State {
+	return actorEntityState{
+		EntityId: e.id,
+
+		Cell:   e.cell,
+		Facing: e.facing.String(),
+
+		bounds: e.Bounds(),
+
+		PathAction: e.pathAction,
+	}
+}
+
+func (e actorEntityState) Id() int64            { return e.EntityId }
+func (e actorEntityState) Bounds() coord.Bounds { return e.bounds }
+func (e actorEntityState) IsDifferentFrom(other entity.State) bool {
+	o := other.(actorEntityState)
+
+	switch {
+	case e.Facing != o.Facing:
+		return true
+
+	case e.PathAction != nil && o.PathAction != nil:
+		if *e.PathAction != *o.PathAction {
+			return true
+		}
+
+	case e.PathAction == nil && o.PathAction != nil:
+		return true
+	case e.PathAction != nil && o.PathAction == nil:
+		return true
+	}
+
+	return false
 }
 
 func (a *actorConn) startIO() {
 	// Setup communication channels
 	cmdCh := make(chan actorCmd)
 	cmdReqCh := make(chan actorCmdRequest)
-	outputCh := make(chan interface{})
+	outputCh := make(chan *rpg2d.WorldState)
 	stopCh := make(chan chan<- struct{})
 
 	// Set the channels accessible to the outside world
@@ -98,7 +147,7 @@ func (a *actorConn) startIO() {
 	// Establish the channel endpoints used inside the go routine
 	var newCmd <-chan actorCmd
 	var sendCmdReq chan<- actorCmdRequest
-	var newState <-chan interface{}
+	var newState <-chan *rpg2d.WorldState
 	var stopReq <-chan chan<- struct{}
 
 	newCmd = cmdCh
@@ -173,8 +222,10 @@ func (a *actorConn) startIO() {
 		// 2. ReadCmdRequest() method requests the actor command request.. again!
 		// 3. stopIO() method has been called
 		select {
-		case _ = <-newState:
-			// TODO send state out over connection
+		case state := <-newState:
+			if state != nil {
+				a.SendJson("update", state)
+			}
 
 			// Reset the cmdReq object
 			cmdReq = actorCmdRequest{}
@@ -218,9 +269,32 @@ func (c actorConn) SubmitCmd(cmd, params string) error {
 	return nil
 }
 
-func (c actorConn) WriteState(s interface{}) error {
-	c.sendState <- s
-	return nil
+// Culls the world state to the actor's viewport
+func (a *actor) WriteState(state rpg2d.WorldState) {
+	c := a.Cell()
+
+	state = state.Cull(coord.Bounds{
+		c.Add(-26, 26),
+		c.Add(26, -26),
+	})
+
+	a.actorConn.WriteState(state)
+}
+
+// Diffs the world state so only the changes are sent
+func (a *actorConn) WriteState(state rpg2d.WorldState) {
+	diff := a.lastState.Diff(state)
+	a.lastState = state
+
+	// Will need this when I start comparing for terrain type changes
+	// a.lastState.Prepare()
+
+	if len(diff.Entities) > 0 || len(diff.Removed) > 0 || diff.TerrainMap != nil {
+		diff.Prepare()
+		a.sendState <- &diff
+	} else {
+		a.sendState <- nil
+	}
 }
 
 func (a actorConn) stopIO() {
