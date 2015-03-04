9303e02699c26c6d959f7bebcc8557c8670add11
Willa Walthall <ghthor@gmail.com>
2015-03-03 22:51:02 -0500

    [game] process chat requests and create chat commands

--- game/muxer.go
+++ game/muxer.go
@@ -10,9 +10,11 @@ type actorConn struct {
 	// Comm interface to muxer used by SubmitCmd() method
 	submitMoveRequest chan<- moveRequest
 	submitUseRequest  chan<- useRequest
+	submitChatRequest chan<- chatRequest
 
 	readMoveCmd <-chan *moveCmd
 	readUseCmd  <-chan *useCmd
+	readChatCmd <-chan *chatCmd
 
 	// Comm interface to muxer used by SendState() method
 	sendState chan<- *rpg2d.WorldState
@@ -34,9 +36,11 @@ func (a *actorConn) startIO() {
 	// Setup communication channels
 	moveReqCh := make(chan moveRequest)
 	useReqCh := make(chan useRequest)
+	chatReqCh := make(chan chatRequest)
 
 	moveCmdCh := make(chan *moveCmd)
 	useCmdCh := make(chan *useCmd)
+	chatCmdCh := make(chan *chatCmd)
 
 	outputCh := make(chan *rpg2d.WorldState)
 	stopCh := make(chan chan<- struct{})
@@ -44,9 +48,11 @@ func (a *actorConn) startIO() {
 	// Set the channels accessible to the outside world
 	a.submitMoveRequest = moveReqCh
 	a.submitUseRequest = useReqCh
+	a.submitChatRequest = chatReqCh
 
 	a.readMoveCmd = moveCmdCh
 	a.readUseCmd = useCmdCh
+	a.readChatCmd = chatCmdCh
 
 	a.sendState = outputCh
 	a.stop = stopCh
@@ -54,18 +60,22 @@ func (a *actorConn) startIO() {
 	// Establish the channel endpoints used inside the go routine
 	var newMoveRequest <-chan moveRequest
 	var newUseRequest <-chan useRequest
+	var newChatRequest <-chan chatRequest
 
 	var sendMoveCmd chan<- *moveCmd
 	var sendUseCmd chan<- *useCmd
+	var sendChatCmd chan<- *chatCmd
 
 	var newState <-chan *rpg2d.WorldState
 	var stopReq <-chan chan<- struct{}
 
 	newMoveRequest = moveReqCh
 	newUseRequest = useReqCh
+	newChatRequest = chatReqCh
 
 	sendMoveCmd = moveCmdCh
 	sendUseCmd = useCmdCh
+	sendChatCmd = chatCmdCh
 
 	newState = outputCh
 	stopReq = stopCh
@@ -76,6 +86,7 @@ func (a *actorConn) startIO() {
 		cmd := struct {
 			moveCmd *moveCmd
 			useCmd  *useCmd
+			chatCmd *chatCmd
 		}{}
 
 		updateMoveCmdWith := func(r moveRequest) {
@@ -111,16 +122,24 @@ func (a *actorConn) startIO() {
 			}
 		}
 
+		updateChatCmdWith := func(r chatRequest) {
+			chatCmd := chatCmd(r)
+			cmd.chatCmd = &chatCmd
+		}
+
 	unlocked:
 		// # This select prioritizes the following events.
 		// ## 2 potential events to respond to
 		// 1. ReadMoveCmd() method requests the actor's movement cmd
 		// 2. ReadUseCmd() method requests the actor's use cmd
-		// 3. stopIO() method has been called
+		// 3. ReadChatCmd() method requests the actor's chat cmd
+		// 4. stopIO() method has been called
 		select {
 		case sendMoveCmd <- cmd.moveCmd:
 			goto locked
 		case sendUseCmd <- cmd.useCmd:
+			goto locked
+		case sendChatCmd <- cmd.chatCmd:
 			goto locked
 
 		case hasStopped = <-stopReq:
@@ -129,10 +148,11 @@ func (a *actorConn) startIO() {
 		}
 
 		// ## 3 potential events to respond to
-		// 1. SubmitCmd() method has been called with a new move/use request
+		// 1. SubmitCmd() method has been called with a new move/use/chat request
 		// 2. ReadMoveCmd() method requests the actor's movement cmd
 		// 3. ReadUseCmd() method requests the actor's use cmd
-		// 4. stopIO() method has been called
+		// 4. ReadChatCmd() method requests the actor's chat cmd
+		// 5. stopIO() method has been called
 		select {
 		case r := <-newMoveRequest:
 			updateMoveCmdWith(r)
@@ -140,10 +160,15 @@ func (a *actorConn) startIO() {
 		case r := <-newUseRequest:
 			updateUseCmdWith(r)
 			goto unlocked
+		case r := <-newChatRequest:
+			updateChatCmdWith(r)
+			goto unlocked
 
 		case sendMoveCmd <- cmd.moveCmd:
 			goto locked
 		case sendUseCmd <- cmd.useCmd:
+			goto locked
+		case sendChatCmd <- cmd.chatCmd:
 			goto locked
 
 		case hasStopped = <-stopReq:
@@ -159,17 +184,27 @@ func (a *actorConn) startIO() {
 		// 1. WriteState() method has been called with a new world state
 		// 2. ReadMoveCmd() method requests the actor's move command
 		// 3. ReadUseCmd() method requests the actor's use command
-		// 4. stopIO() method has been called
+		// 4. ReadChatCmd() method requests the actor's chat command
+		// 5. stopIO() method has been called
 		select {
 		case state := <-newState:
 			if state != nil {
 				a.SendJson("update", state)
 			}
+
+			// Chat commands get consumed only once,
+			// unlike move and use commands which keep
+			// getting consumed every tick until the client
+			// cancels the command. So we reset it here.
+			cmd.chatCmd = nil
+
 			goto unlocked
 
 		case sendMoveCmd <- cmd.moveCmd:
 			goto locked
 		case sendUseCmd <- cmd.useCmd:
+			goto locked
+		case sendChatCmd <- cmd.chatCmd:
 			goto locked
 
 		case hasStopped = <-stopReq:
