b6b6baa46773e427d929b19cf9604df5e878ccd6
Willa Walthall <ghthor@gmail.com>
2015-03-07 14:16:37 -0500

    [www/js/client] removed unused module

--- www/js/client/environment.js
+++
@@ -1,6 +0,0 @@
-define(["underscore"], function(_) {
-    // This determines what type of socket is used to send key input
-    // browser - WebSocket
-    // native  - UDP
-    return _.isUndefined(window.nativeBridge) ? "browser" : "native";
-});
