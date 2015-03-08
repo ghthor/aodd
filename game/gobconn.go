af0d5a8383503f2fb266653396ad8347d9c403aa
Willa Walthall <ghthor@gmail.com>
2015-03-08 17:26:57 -0400

    [game] implement a conn that en/decodes using gob

---
+++ game/gobconn.go
@@ -0,0 +1,121 @@
+package game
+
+import (
+	"bufio"
+	"encoding/gob"
+	"io"
+
+	"github.com/ghthor/aodd/game/datastore"
+	"github.com/ghthor/engine/rpg2d"
+)
+
+// Used to determine the next type that's in the
+// buffer so we can decode it into a real value.
+// We'll decode an encoded type and switch on its
+// value so we'll have the correct value to decode
+// into.
+type EncodedType int
+
+const (
+	ET_ERROR EncodedType = iota
+
+	ET_REQ_LOGIN
+	ET_REQ_CREATE
+
+	ET_RESP_AUTH_FAILED
+	ET_RESP_ACTOR_EXISTS
+	ET_RESP_ACTOR_DOESNT_EXIST
+
+	ET_RESP_LOGIN_SUCCESS
+	ET_RESP_CREATE_SUCCESS
+
+	ET_WORLD_STATE
+	ET_WORLD_STATE_DIFF
+)
+
+type ReqLogin struct{ Name, Password string }
+type ReqCreate struct{ Name, Password string }
+
+type RespAuthFailed struct{ Name string }
+type RespActorExists struct{ Name string }
+type RespActorDoesntExist struct{ Name, Password string }
+
+type RespLoginSucces struct{ Name string }
+type RespCreateSuccess struct{ Name string }
+
+func init() {
+	// Pre login Request/Response types
+	gob.Register(ReqLogin{})
+	gob.Register(ReqCreate{})
+
+	gob.Register(RespAuthFailed{})
+	gob.Register(RespActorExists{})
+	gob.Register(RespActorDoesntExist{})
+
+	gob.Register(RespLoginSucces{})
+	gob.Register(RespCreateSuccess{})
+
+	// Engine types
+	gob.Register(rpg2d.WorldState{})
+	gob.Register(rpg2d.WorldStateDiff{})
+	gob.Register(rpg2d.TerrainMapState{})
+
+	// Entity states
+	gob.Register(ActorEntityState{})
+	gob.Register(SayEntityState{})
+	gob.Register(AssailEntityState{})
+}
+
+type GobConn interface {
+	EncodeAndSend(EncodedType, interface{}) error
+	ReadNextType() (EncodedType, error)
+	Decode(interface{}) error
+}
+
+type gobConn struct {
+	enc  *gob.Encoder
+	wbuf *bufio.Writer
+
+	*gob.Decoder
+}
+
+func (c gobConn) EncodeAndSend(t EncodedType, ev interface{}) error {
+	err := c.enc.Encode(t)
+	if err != nil {
+		return err
+	}
+
+	err = c.enc.Encode(ev)
+	if err != nil {
+		return err
+	}
+
+	return c.wbuf.Flush()
+}
+
+func (c gobConn) ReadNextType() (t EncodedType, err error) {
+	err = c.Decoder.Decode(&t)
+	return
+}
+
+func NewGobConn(rw io.ReadWriter) GobConn {
+	wbuf := bufio.NewWriter(rw)
+	enc := gob.NewEncoder(wbuf)
+	dec := gob.NewDecoder(rw)
+
+	return gobConn{
+		enc:  enc,
+		wbuf: wbuf,
+
+		Decoder: dec,
+	}
+}
+
+type serverConn struct {
+	GobConn
+
+	sim       rpg2d.RunningSimulation
+	datastore datastore.Datastore
+
+	actor *actor
+}
---
+++ game/gobconn_test.go
@@ -0,0 +1,48 @@
+package game_test
+
+import (
+	"bytes"
+
+	"github.com/ghthor/aodd/game"
+	"github.com/ghthor/engine/rpg2d"
+	"github.com/ghthor/engine/rpg2d/entity"
+
+	"github.com/ghthor/gospec"
+	. "github.com/ghthor/gospec"
+)
+
+func DescribeGobConn(c gospec.Context) {
+	c.Specify("a gob conn", func() {
+		buf := bytes.NewBuffer(make([]byte, 0, 1024))
+		gobconn := game.NewGobConn(buf)
+
+		c.Specify("can send", func() {
+			c.Specify("world states w/ entities", func() {
+				worldState := rpg2d.WorldState{
+					Entities: entity.StateSlice{
+						game.ActorEntityState{EntityId: 2},
+						game.SayEntityState{EntityId: 3},
+						game.AssailEntityState{EntityId: 4},
+					},
+				}
+
+				c.Expect(gobconn.EncodeAndSend(game.ET_WORLD_STATE, worldState), IsNil)
+
+				eType, err := gobconn.ReadNextType()
+				c.Assume(err, IsNil)
+				c.Expect(eType, Equals, game.ET_WORLD_STATE)
+
+				c.Specify("and can recv", func() {
+					var decodedState rpg2d.WorldState
+					c.Expect(gobconn.Decode(&decodedState), IsNil)
+					c.Expect(decodedState.Entities[0], Equals, game.ActorEntityState{EntityId: 2})
+					c.Expect(decodedState.Entities[1], Equals, game.SayEntityState{EntityId: 3})
+					c.Expect(decodedState.Entities[2], Equals, game.AssailEntityState{EntityId: 4})
+				})
+			})
+
+			c.Specify("world state diffs w/ entities", func() {
+			})
+		})
+	})
+}
